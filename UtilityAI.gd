6bca93bf733a350f701ed2ecb52aec804b9d4139
Cammy <cameronsteele@steelecameron.com>
2023-04-30 16:17:01 -0700

    add delta seconds to score method calls

--- UtilityAI.gd
+++ UtilityAI.gd
@@ -1,97 +1,103 @@
-extends Node;
-class_name UtilityAI;
+extends Node
+class_name UtilityAI
 
 ## The amount of time between ticks of the mind.
 var tickInterval = 1; # In seconds
 
 ## The amount of time that has passed since the last tick of the mind.
-var tickDeltaSeconds = 0;
+var delta_seconds = 0
 
 ## The behaviors that the mind can choose from.
-var behaviors = [];
+var behaviors = []
 
 ## The behavior that is currently being performed.
 var activeBehavior: Behavior; #@TODO probably pluralize and have behaviors that can be non-exclusive
 
 func _ready():
-	pass;
+	pass
 
-func _process(deltaSeconds):
-	tickDeltaSeconds += deltaSeconds;
+func _process(deltaSeconds: float):
+	delta_seconds += deltaSeconds
 
-	if(tickDeltaSeconds > tickInterval):
+	if(delta_seconds > tickInterval):
 		#@REVISIT architecture:
 		## Run user defined callback:
-		_tickMind(tickDeltaSeconds);
+		_tickMind(delta_seconds)
 
 		## Run internal callback:
-		tickMind(tickDeltaSeconds);
+		tickMind(delta_seconds)
 
-		tickDeltaSeconds = 0;
+		delta_seconds = 0
 
-func _tickMind(tickDeltaSeconds: float) -> void:
-	pass;
+func _tickMind(delta_seconds: float) -> void:
+	pass
 
-func tickMind(tickDeltaSeconds):
-	var priorityScores = contemplatePriorities();
-	if(priorityScores.size()):
+func tickMind(delta_seconds: float):
+	var behavior_scores = contemplate_priorities(delta_seconds)
+
+	if(behavior_scores.size()):
 		# Whether we've found a behavior that consumes all attention: #@REVISIT architecture
-		var fullyOccupied = false;
+		var fullyOccupied = false
 
 		var priority = 0; #@RENAME to make clearer
 
-		while(!fullyOccupied && priority < priorityScores.size()):
+		while(!fullyOccupied && priority < behavior_scores.size()):
 			# If we did not run the behavior last frame #@REVISIT architecture
-			var initializingBehavior: bool = (activeBehavior != priorityScores[priority]["behavior"]);
+			var initializingBehavior: bool = (activeBehavior != behavior_scores[priority]["behavior"])
 			if(initializingBehavior):
 				# If had a behavior last tick and it has exit method, run it
 				if(activeBehavior):
 					if(activeBehavior.exit):
-						activeBehavior.exit.call();
+						activeBehavior.exit.call()
 
 				# Set the new active behavior
-				activeBehavior = priorityScores[priority]["behavior"];
+				activeBehavior = behavior_scores[priority]["behavior"]
 
 				# If the new behavior has an enter function, run it
 				if(activeBehavior.enter):
-					activeBehavior.enter.call();
+					activeBehavior.enter.call()
 
 			# Run the behavior's tick method
-			var behaviorReturn = activeBehavior.tick.call(tickDeltaSeconds);
+			var behaviorReturn = activeBehavior.tick.call(delta_seconds)
 
 			# If behaviorReturn is an instance of BehaviorResult
 			#@REVISIT should it not always be BehaviorResult
 			if(behaviorReturn is BehaviorResult):
 				# If the behavior wants to consume all attention
-				fullyOccupied = behaviorReturn.exclusive;
+				fullyOccupied = behaviorReturn.exclusive
 
 				# If the behavior wants to transition to another behavior
 				if(behaviorReturn.transitionTo):
-					activeBehavior = behaviorReturn.transitionTo;
+					activeBehavior = behaviorReturn.transitionTo
 
 			# If behaviorReturn is not an instance of BehaviorResult
 			else:
 				# Assume behaviorReturn is a boolean representing fullyOccupied
-				fullyOccupied = behaviorReturn;
+				fullyOccupied = behaviorReturn
 
-			priority += 1;
+			priority += 1
 
 #@TODO moving to this architecture
 func add_behavior(behavior: Behavior):
-	behaviors.push_back(behavior);
+	behaviors.push_back(behavior)
 
-func contemplatePriorities():
-	var priorityScores = []
+func contemplate_priorities(delta_seconds: float):
+	var behavior_scores = []
 
 	for behavior in behaviors:
-		var score = behavior.score.call()
-		priorityScores.push_back({
+		# Run the behavior's score function
+		var score = behavior.score.call(delta_seconds)
+
+		# Add score and behavior to the behavior_scores array
+		behavior_scores.push_back({
 			"score": score,
 			"behavior": behavior
 		})
 
-	priorityScores.sort_custom(sort_scores_descending)
-	return priorityScores
+	# Sort the behavior_scores array by score
+	behavior_scores.sort_custom(sort_scores_descending)
+
+	return behavior_scores
 
 static func sort_scores_descending(a, b):
 	if a["score"] > b["score"]:
@@ -132,22 +138,23 @@ class Behavior:
 		self.tick = tick
 		self.enter = enter
 		self.exit = exit
-		self.priorityLevel = priorityLevel;
+		self.priorityLevel = priorityLevel
 
 class BehaviorResult:
 	## Whether the behavior is exclusive and should consume all attention.
-	var exclusive: bool;
+	var exclusive: bool
 
 	## The behavior to transition to.
-	var transitionTo: Behavior;
+	var transitionTo: Behavior
 
 	func _init(exclusive = false, transitionTo = null):
-		self.exclusive = exclusive;
-		self.transitionTo = transitionTo;
+		self.exclusive = exclusive
+		self.transitionTo = transitionTo
 
 #@REVISIT architecture
 static func NewBehaviorResult(params: Dictionary = {}):
-	var exclusive = params.get("exclusive", true);
-	var transitionTo = params.get("transitionTo", null);
+	var exclusive = params.get("exclusive", true)
+	var transitionTo = params.get("transitionTo", null)
 
-	return BehaviorResult.new(exclusive, transitionTo);
+	return BehaviorResult.new(exclusive, transitionTo)
+
